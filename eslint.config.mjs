4c39511541ff797be5f62728ed7549bf8352b293
JulienDroulez31 <julien.droulez31@gmail.com>
2024-11-15 22:56:32 +0100

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,17 @@ import pluginJs from "@eslint/js";
 
 export default [
   {
-    ignores: ["node_modules/**", "**/*.test.js"],
+    ignores: ["**/*.test.js"],
     files: ["**/*.js"],
     languageOptions: { sourceType: "commonjs" },
   },
-  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: { ...globals.node, ...globals.jest } } },  // Ajout de globals.jest
   pluginJs.configs.recommended,
   {
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
-      "no-console": "warn", // Avertissement pour console.log
-      "constructor-super": "off", // Désactive cette règle si elle pose problème
+      "no-console": "error",
     },
   },
 ];
